c09fbc8ab76a07adee17e9106963273c25a7bc97
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2026-01-06 17:55:42 +0900

    feat: QueryClientProvider 추가 #23

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '@/lib/api/query-client.ts';
 import LoginPage from '@/app/(auth)/login/page';
 import RegisterPage from '@/app/(auth)/register/page';
 import { ProtectedRoute } from '@/domains/auth/components';
@@ -7,16 +9,17 @@ import DashboardPage from './app/dashboard/page';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<PageLayout />}>
-          {/* 공개 라우트 */}
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<PageLayout />}>
+            {/* 공개 라우트 */}
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
 
-          {/* 인증 필요 라우트 */}
-          {/** <Route
+            {/* 인증 필요 라우트 */}
+            {/** <Route
             path="/dashboard"
             element={
               <ProtectedRoute>
@@ -25,12 +28,13 @@ function App() {
             }
           />  */}
 
-          {/* 기본 리다이렉트 */}
-          <Route path="/" element={<Navigate to="/dashboard" replace />} />
-          <Route path="*" element={<Navigate to="/dashboard" replace />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+            {/* 기본 리다이렉트 */}
+            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            <Route path="*" element={<Navigate to="/dashboard" replace />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
